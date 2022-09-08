3f492b9272580428e61ad7407837225ae5b6d07e
Pol Henarejos <pol.henarejos@cttc.es>
2022-09-08 13:45:30 +0200

    Upgrade version to v3.2.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/hsm_version.h
+++ src/hsm_version.h
@@ -18,7 +18,7 @@
 #ifndef __VERSION_H_
 #define __VERSION_H_
 
-#define HSM_SDK_VERSION 0x0300
+#define HSM_SDK_VERSION 0x0302
 
 #define HSM_SDK_VERSION_MAJOR ((HSM_SDK_VERSION >> 8) & 0xff)
 #define HSM_SDK_VERSION_MINOR (HSM_SDK_VERSION & 0xff)

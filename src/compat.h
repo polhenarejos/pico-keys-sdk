63a2546166d742a26aa4bcd36f4bc10c5c183212
Pol Henarejos <pol.henarejos@cttc.es>
2024-01-02 20:36:48 +0100

    Fix CodeQL build.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/compat.h
+++ src/compat.h
@@ -18,12 +18,10 @@
 #ifndef _COMPAT_H_
 #define _COMPAT_H_
 
-#ifdef __GNUC__
-#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
-#endif
-
 #ifdef _MSC_VER
 #define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
+#else
+#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
 #endif
 
 #ifdef __cplusplus

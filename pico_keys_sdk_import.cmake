b5c2e55c7196a2e12429b30b971e152001588eae
Pol Henarejos <pol.henarejos@cttc.es>
2026-01-22 12:09:27 +0100

    Add missing files.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- pico_keys_sdk_import.cmake
+++ pico_keys_sdk_import.cmake
@@ -253,9 +253,12 @@ set(MBEDTLS_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/poly1305.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/ripemd160.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/des.c
+    ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/x509write.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/x509write_crt.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/x509_create.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/x509write_csr.c
+    ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/base64.c
+    ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/pem.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/pk.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/pk_wrap.c
     ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/pkwrite.c
@@ -374,6 +377,7 @@ if(USB_ITF_HID)
         ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/pk_wrap.c
         ${CMAKE_CURRENT_LIST_DIR}/mbedtls/library/pkwrite.c
     )
+
 endif()
 
     set(CBOR_SOURCES

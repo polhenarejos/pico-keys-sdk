3836ee70e4100dd527096c6fd0a52360efeb0473
Pol Henarejos <pol.henarejos@cttc.es>
2026-04-20 23:49:42 +0200

    Do not enable INT CCID with LWIP.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/usb/usb.h
+++ src/usb/usb.h
@@ -76,7 +76,7 @@ enum {
     REPORT_ID_COUNT
 };
 
-#if defined(ESP_PLATFORM) && defined(USB_ITF_HID) && defined(USB_ITF_CCID)
+#if defined(ESP_PLATFORM) && (defined(USB_ITF_HID) || defined(USB_ITF_LWIP))&& defined(USB_ITF_CCID)
 #define TUSB_SMARTCARD_CCID_EPS 2
 #else
 #define TUSB_SMARTCARD_CCID_EPS 3
